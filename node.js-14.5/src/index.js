1729eaa65956ec042a06a964121e0caf8b22abc7
Ümit Çakmak <umit.cakmak@altogic.com>
2022-09-30 13:58:16 +0300

    node.js templates updated

--- node.js-14.5/src/index.js
+++ node.js-14.5/src/index.js
@@ -34,42 +34,42 @@ const { createClient } = require("altogic");
             }
 
     session - session of the user making the request
-              {
-                userId: "62602c378415c0a0519102aa",
-                token:
-                  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZJZCI6IjYyNjAyYzM3ODQxNWMwYTA1MTkxMDJiNiIsInVzZXJJZCI6IjYyNjAyYzM3ODQxNWMwYTA1MTkxMDJhYSIsInV1aWQiOiI2NmMxM2JhZS1iOGE1LTQ1NDItOWJkZC0zYmE4NTAwZTU2NjUiLCJpYXQiOjE2NjM2ODYxNTB9.LAbzFnk59Ix8z-6Wkcd3Gz49FWnswtvuGFCqG-AX5fU",
-                creationDtm: "2022-09-20T15:02:30.331Z",
-                userAgent: {
-                  family: "Chrome",
-                  major: "105",
+            {
+              userId: "62602c378415c0a0519102aa",
+              token:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZJZCI6IjYyNjAyYzM3ODQxNWMwYTA1MTkxMDJiNiIsInVzZXJJZCI6IjYyNjAyYzM3ODQxNWMwYTA1MTkxMDJhYSIsInV1aWQiOiI2NmMxM2JhZS1iOGE1LTQ1NDItOWJkZC0zYmE4NTAwZTU2NjUiLCJpYXQiOjE2NjM2ODYxNTB9.LAbzFnk59Ix8z-6Wkcd3Gz49FWnswtvuGFCqG-AX5fU",
+              creationDtm: "2022-09-20T15:02:30.331Z",
+              userAgent: {
+                family: "Chrome",
+                major: "105",
+                minor: "0",
+                patch: "0",
+                device: {
+                  family: "Other",
+                  major: "0",
                   minor: "0",
                   patch: "0",
-                  device: {
-                    family: "Other",
-                    major: "0",
-                    minor: "0",
-                    patch: "0",
-                  },
-                  os: {
-                    family: "Mac OS X",
-                    major: "10",
-                    minor: "15",
-                    patch: "7",
-                  },
                 },
-                accessGroupKeys: ['admin', 'member'],
-              }
+                os: {
+                  family: "Mac OS X",
+                  major: "10",
+                  minor: "15",
+                  patch: "7",
+                },
+              },
+              accessGroupKeys: ['admin', 'member'],
+            }
 
     appInfo - contextual information about the app and the environment 
-              {
-                appName: "Practice App",
-                environmentName: "Development",
-                environmentType: "trial",
-                apiBaseUrl: "https://c1-na.altogic.com/e:62602c378415c0a0519102b6",
-                executionStartDtm: "2022-09-20T15:02:30.325Z",
-              }
+            {
+              appName: "Practice App",
+              environmentName: "Development",
+              environmentType: "trial",
+              apiBaseUrl: "https://c1-na.altogic.com/e:62602c378415c0a0519102b6",
+              executionStartDtm: "2022-09-20T15:02:30.325Z",
+            }
 
-    body - request body JSON object
+    body - request body JSON object. This can be single JSON object or an array of JSON objects.
 
   res variable has:
     send(text, status) - Function to return text response. Status code defaults to 200
@@ -78,13 +78,26 @@ const { createClient } = require("altogic");
   If an exception is thrown, a response with code 500 is returned.
 */
 
-const ENV_URL = "<my app environment url>";
-const CLIENT_KEY = "<my app client key>";
+/*
+  In order to use the Altogic client library you need to create an app and a client key in Altogic. Additionally, if you will be using the Authentication module of this library, you might need to do additional configuration in your app settings. 
+  
+  As input to createClient you need to provide your environement base URL and client-key. You can create a new environment or access your app envUrl from the Environments view and create a new clientKey from App Settings/Client library view in Altogic Designer.
+
+  Please set ENV_URL and CLIENT_KEY values below if you plan to use Altogic Client library in this function.
+*/
+const ENV_URL = null;
+const CLIENT_KEY = null;
 
 module.exports = async function (req, res) {
 	// Create a client for interacting with your backend app
 	// You need to provide environment url and client key as input parameters
-	const altogic = createClient(ENV_URL, CLIENT_KEY);
+	let altogic;
+
+	if (!ENV_URL || !CLIENT_KEY) {
+		console.warn(
+			"Client library environment URL and/or client key variables are not set. Unless these variables are set, the cloud function cannot use Altotic Client Library."
+		);
+	} else altogic = createClient(ENV_URL, CLIENT_KEY);
 
 	res.json({
 		quote: "Hello world!",
