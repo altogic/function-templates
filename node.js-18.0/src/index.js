c8f374d5e7ca169cf5326be8014fcdea479a879a
Ümit Çakmak <umit.cakmak@altogic.com>
2022-10-19 15:28:50 +0300

    parameter definitions updated

--- node.js-18.0/src/index.js
+++ node.js-18.0/src/index.js
@@ -1,87 +1,34 @@
 const { createClient } = require("altogic");
 
 /*
-  req variable has:
-    ids - endpoint path id parameters object
-            {
-              userId: '624b52783c77af658ac14ea2', 
-              productId: '6151be8581f301001aa73c3c'
-            }
+  Please see README.md for detailed information about request input parameters
 
-    query - reqeust query string parameters
-            {
-              username: 'user123', 
-              tags: ['tag1', 'tag2']
-            }
-    
-    headers - request headers object 
-            {
-              token: 'tk_23423...', 
-              'content-type': 'application/json'
-            }
-
-    appParams - App parameters object
-            {
-              param1: 123, 
-              param2: true, 
-              param3: 'text value'
-            }
-
-    client - request callers device type and IP information, 
-            {
-              ip: '100.123.45.66', 
-              deviceType: 'desktop'
-            }
-
-    session - session of the user making the request
-            {
-              userId: "62602c378415c0a0519102aa",
-              token:
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZJZCI6IjYyNjAyYzM3ODQxNWMwYTA1MTkxMDJiNiIsInVzZXJJZCI6IjYyNjAyYzM3ODQxNWMwYTA1MTkxMDJhYSIsInV1aWQiOiI2NmMxM2JhZS1iOGE1LTQ1NDItOWJkZC0zYmE4NTAwZTU2NjUiLCJpYXQiOjE2NjM2ODYxNTB9.LAbzFnk59Ix8z-6Wkcd3Gz49FWnswtvuGFCqG-AX5fU",
-              creationDtm: "2022-09-20T15:02:30.331Z",
-              userAgent: {
-                family: "Chrome",
-                major: "105",
-                minor: "0",
-                patch: "0",
-                device: {
-                  family: "Other",
-                  major: "0",
-                  minor: "0",
-                  patch: "0",
-                },
-                os: {
-                  family: "Mac OS X",
-                  major: "10",
-                  minor: "15",
-                  patch: "7",
-                },
-              },
-              accessGroupKeys: ['admin', 'member'],
-            }
-
-    appInfo - contextual information about the app and the environment 
-            {
-              appName: "Practice App",
-              environmentName: "Development",
-              environmentType: "trial",
-              apiBaseUrl: "https://c1-na.altogic.com/e:62602c378415c0a0519102b6",
-              executionStartDtm: "2022-09-20T15:02:30.325Z",
-            }
-
-    body - request body JSON object. This can be single JSON object or an array of JSON objects.
+  req variable has the following values, please note that these values can change.
+      ids - endpoint path id parameters object
+      query - reqeust query string parameters
+      headers - request headers object 
+      appParams - App parameters object
+      client - request callers device type and IP information, 
+      session - session of the user making the request
+      appInfo - contextual information about the app and the environment 
+      files - array of file objects
+      body - request body JSON object. This can be a single JSON object or an array of JSON objects.
 
   res variable has:
-    send(text, status) - Function to return text response. Status code defaults to 200
-    json(object, status) - Function to return JSON response. Status code defaults to 200
+      send(text, status) - Function to return text response. Status code defaults to 200
+      json(object, status) - Function to return JSON response. Status code defaults to 200
 
   If an exception is thrown, a response with code 500 is returned.
 */
 
 /*
-  In order to use the Altogic client library you need to create an app and a client key in Altogic. Additionally, if you will be using the Authentication module of this library, you might need to do additional configuration in your app settings. 
+  In order to use the Altogic client library you need to create an app and a client key in Altogic. 
+  Additionally, if you will be using the Authentication module of this library, you might need to do 
+  additional configuration in your app settings. 
   
-  As input to createClient you need to provide your environement base URL and client-key. You can create a new environment or access your app envUrl from the Environments view and create a new clientKey from App Settings/Client library view in Altogic Designer.
+  As input to createClient you need to provide your environement base URL and client-key. You can create
+  a new environment or access your app envUrl from the Environments view and create a new clientKey from 
+  App Settings/Client library view in Altogic Designer.
 
   Please set ENV_URL and CLIENT_KEY values below if you plan to use Altogic Client library in this function.
 */
@@ -95,7 +42,7 @@ module.exports = async function (req, res) {
 
 	if (!ENV_URL || !CLIENT_KEY) {
 		console.warn(
-			"Client library environment URL and/or client key variables are not set. Unless these variables are set, the cloud function cannot use Altotic Client Library."
+			"Client library environment URL and/or client key variables are not set. Unless these variables are set, the cloud function cannot use Altogic Client Library."
 		);
 	} else altogic = createClient(ENV_URL, CLIENT_KEY);
 
